a3238c5189ea37158671af9d9d4ad055ba6177b7
Britnay Gore <britnaygore268@gmail.com>
2024-07-15 20:24:47 -0400

    Condensed code to be more efficient

--- Program.cs
+++ Program.cs
@@ -1,160 +1,103 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(@"Choose a difficulty level by number:
-1. Easy
-2. Medium
-3. Hard
-4. Cheater");
-
-int choice = int.Parse(Console.ReadLine());
-
-switch (choice)
+try
 {
-    case 1:
-        Easy();
-        break;
-    case 2:
-        Medium();
-        break;
-    case 3:
-        Hard();
-        break;
-    case 4:
-        Cheater();
-        break;
-    default:
-        Console.WriteLine("Enter a valid option");
-        break;
+    bool validInput = false;
+
+    while (!validInput)
+    {
+        Console.WriteLine(@"Choose a difficulty level by number:
+        1. Easy
+        2. Medium
+        3. Hard
+        4. Cheater");
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Easy();
+                validInput = true;
+                break;
+            case 2:
+                Medium();
+                validInput = true;
+                break;
+            case 3:
+                Hard();
+                validInput = true;
+                break;
+            case 4:
+                Cheater();
+                validInput = true;
+                break;
+            default:
+                Console.WriteLine("Enter a number from the available option");
+                break;
+        }
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Please enter a number");
+}
+
+
+void GuessingGame(int maximumTries)
+{
+    Console.WriteLine("Guess the secret number!");
+
+    Random random = new Random();
+
+    int secretNumber = random.Next(1, 100);
+
+    int tries = 0;
+
+    while (tries < maximumTries || maximumTries == 0)
+    {
+        tries++;
+        Console.Write($"Your guess ({tries})> ");
+
+        try
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input == secretNumber)
+            {
+                Console.WriteLine("Success! You've guessed right!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine(@$"You guess is {(input > secretNumber ? "too high" : "too low")}! {(maximumTries == 0 ? "": $"You have {maximumTries - tries} guesses left!")}");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a number");
+        }
+
+    }
+
+    if (tries == maximumTries)
+        Console.WriteLine($"Ooops - You've run out of guesses! Secret Number was: {secretNumber}");
+
+    Console.Read();
 }
 
 void Easy()
 {
-    Console.WriteLine("Guess the secret number!");
-
-    Random random = new Random();
-
-    int secretNumber = random.Next(1, 100);
-
-    int tries = 0;
-
-    while (tries < 8)
-    {
-        tries++;
-        Console.Write($"Your guess ({tries})> ");
-
-        int input = int.Parse(Console.ReadLine());
-
-        if (input == secretNumber)
-        {
-            Console.WriteLine("Success! You've guessed right!");
-            break;
-        }
-        else
-        {
-            Console.WriteLine(@$"You guess is {(input > secretNumber ? "too high" : "too low")}! You have {8 - tries} guesses left!");
-        }
-    }
-
-    if (tries == 8)
-        Console.WriteLine($"Ooops - You've run out of guesses! Secret Number was: {secretNumber}");
-
-    Console.Read();
+    GuessingGame(8);
 }
 
 void Medium()
 {
-    Console.WriteLine("Guess the secret number!");
-
-    Random random = new Random();
-
-    int secretNumber = random.Next(1, 100);
-
-    int tries = 0;
-
-    while (tries < 6)
-    {
-        tries++;
-        Console.Write($"Your guess ({tries})> ");
-
-        int input = int.Parse(Console.ReadLine());
-
-        if (input == secretNumber)
-        {
-            Console.WriteLine("Success! You've guessed right!");
-            break;
-        }
-        else
-        {
-            Console.WriteLine(@$"You guess is {(input > secretNumber ? "too high" : "too low")}! You have {6 - tries} guesses left!");
-        }
-    }
-
-    if (tries == 6)
-        Console.WriteLine($"Ooops - You've run out of guesses! Secret Number was: {secretNumber}");
-
-    Console.Read();
+    GuessingGame(6);
 }
 
 void Hard()
 {
-    Console.WriteLine("Guess the secret number!");
-
-    Random random = new Random();
-
-    int secretNumber = random.Next(1, 100);
-
-    int tries = 0;
-
-    while (tries < 4)
-    {
-        tries++;
-        Console.Write($"Your guess ({tries})> ");
-
-        int input = int.Parse(Console.ReadLine());
-
-        if (input == secretNumber)
-        {
-            Console.WriteLine("Success! You've guessed right!");
-            break;
-        }
-        else
-        {
-            Console.WriteLine(@$"You guess is {(input > secretNumber ? "too high" : "too low")}! You have {4 - tries} guesses left!");
-        }
-    }
-
-    if (tries == 4)
-        Console.WriteLine($"Ooops - You've run out of guesses! Secret Number was: {secretNumber}");
-
-    Console.Read();
+    GuessingGame(4);
 }
 
 void Cheater()
 {
-    Console.WriteLine("Guess the secret number!");
-
-    Random random = new Random();
-
-    int secretNumber = random.Next(1, 100);
-
-    int tries = 0;
-
-    while (true)
-    {
-        tries++;
-        Console.Write($"Your guess ({tries})> ");
-
-        int input = int.Parse(Console.ReadLine());
-
-        if (input == secretNumber)
-        {
-            Console.WriteLine("Success! You've guessed right!");
-            break;
-        }
-        else
-        {
-            Console.WriteLine(@$"You guess is {(input > secretNumber ? "too high" : "too low")}!");
-        }
-    }
-
-    Console.Read();
+    GuessingGame(0);
 }
